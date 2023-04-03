ecbce19fd901975569c32c3b2129383d9385f3bb
ankan20 <127923458+ankan20@users.noreply.github.com>
2023-04-03 20:05:09 +0530

    Update sqrtbyBinarysearch.cpp

--- sqrtbyBinarysearch.cpp
+++ sqrtbyBinarysearch.cpp
@@ -25,11 +25,25 @@ long long int binarysearch(int n){
     }
     return ans;
 }
+double moreprecision(int n,int precision,int tempsol){
+    double factor=1;
+    double ans =tempsol;
+
+    for(int i=0;i<precision;i++){
+        factor=factor/10;
+
+        for(double j=ans;j*j<n;j=j+factor){
+            ans=j;
+        }
+    }
+    return ans;
+}
 int main(){
 
 int n;
 cout<<"give a number :";
 cin>>n;
-cout<<"square root of this number is "<<binarysearch(n);
+int tempsol=binarysearch(n);
+cout<<"square root of this number is "<<moreprecision(n,3,tempsol);
     return 0;
 }
